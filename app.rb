48554e3c7c05c34d2c2c6860fc6a71f31788108c
Santato7 <santatolucas7@gmail.com>
2025-05-14 11:46:22 -0300

    feat: add Swagger UI documentation

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require 'sinatra'
 require_relative './routes/matches'
 require_relative './routes/players'
 require_relative './routes/setup'
+
+get '/docs' do
+  redirect to('/docs/index.html')
+end
+
+get '/swagger.yaml' do
+  content_type 'application/yaml'
+  File.read('swagger.yaml')
+end
